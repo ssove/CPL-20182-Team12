9ef1dd98b4061c4e738a8008ff7c9b1b15f61618
JeongWoo Woo <dnwjddn52@gmail.com>
2018-10-06 18:36:14 +0900

    File path reorganization
    Signed-off-by: JeongWoo Woo <dnwjddn52@gmail.com>

---
+++ kubernetes/kubernetes_common_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# You should run it as root.
+
+swapoff -a
+
+apt-get update
+apt-get install -y docker-engine
+
+apt-get update && apt-get install -y apt-transport-https curl
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add –
+
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y kubernetes-cni=0.6.0-00
+apt-get install -y kubelet=1.12.1-02 kubeadm=1.12.1-02 kubectl=1.12.1-02
+
+systemctl daemon-reload
+systemctl restart kubelet
